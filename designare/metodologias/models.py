8545f648b3c8f6413fb8cf2ba70f0f841910e888
henrvelho <henrvelho@gmail.com>
2016-09-10 21:11:41 -0300

    adicionado modelos da metodologia

--- designare/metodologias/models.py
+++ designare/metodologias/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Metodologia(models.Model):
+	nome = models.
+	etapa_inicial = 
+
+class Etapa(models.Model):
+	nome = models.
+	descricao = models.
+	prox_etapa = models.
+	atividade_inicial = models.
+
+
+#class Projeto(models.Model):
+#	nome = models.
+#	descricao = models.
+#	metodologia = models.
+#	dt_inicio = models.
+#	dt_fim = models.
+#	capa = models.
+#	estado = models.
+
+class Atividade(models.Model):
+	nome = models.
+	descricao = models.
+	prox_atividade = models.
+
+#class Execução(models.Model):
+#	data = models.
+#	usuario = models.
+#   recurso = models.
+
+#class Recurso(models.Model)
+#	acao = models.
+#	nome = models.
