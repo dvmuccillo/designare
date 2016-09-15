16daf241e63ac99a1cb589c3a428d1f149408a20
henrvelho <henrvelho@gmail.com>
2016-09-15 12:34:12 -0300

    Update models.py

--- designare/metodologias/models.py
+++ designare/metodologias/models.py
@@ -3,22 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Metodologia(models.Model):
-	nome = models.CharField(max_length=100)
+	nome = models.CharField(max_length=50)
 	etapa_inicial = models.ForeignKey(Etapa,on_delete=)
 
 class Etapa(models.Model):
-	nome = models.CharField(max_length=100)
-	descricao = models.CharField(max_length=250)
+	nome = models.CharField(max_length=50)
+	descricao = models.CharField(max_length=300)
 	prox_etapa = models.ForeignKey('self',on_delete=)
 	atividade_inicial = models.ForeignKey(Atividade,on_delete=)
+	metodologia = models.ForeignKey(Metodologia,on_delete=models.CASCADE)
 	
 	def get_proxima_etapa_valida():
 		return 
 
 class Atividade(models.Model):
-	nome = models.CharField(max_length=100)
-	descricao = models.CharField(max_length=250)
+	nome = models.CharField(max_length=50)
+	descricao = models.CharField(max_length=300)
 	prox_atividade = models.ForeignKey('self',on_delete=)
+	etapa = models.ForeignKey(Etapa,on_delete=models.CASCADE)
+	
 	
 #class Projeto(models.Model):
 #	nome = models.
