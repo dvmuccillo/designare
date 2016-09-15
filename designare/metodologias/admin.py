b02e48f0f5c1967c1c8938ce2c73eb08bdb99c0a
Daniel Vargas Muccillo <daniel.vargasmuccillo@gmail.com>
2016-09-15 13:14:28 -0300

    Registrado modelos de metodologia para interface administrativa.

--- designare/metodologias/admin.py
+++ designare/metodologias/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from models import Metodologia, Etapa, Atividade
 # Register your models here.
+admin.site.register(Metodologia)
+admin.site.register(Etapa)
+admin.site.register(Atividade)
