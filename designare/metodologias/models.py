c7d075dfc41272eefed34e9006e54f93e1302982
Daniel Vargas Muccillo <daniel.vargasmuccillo@gmail.com>
2016-09-16 15:50:40 -0300

    Metodologia: correções nas relações entre classes.

--- designare/metodologias/models.py
+++ designare/metodologias/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Metodologia(models.Model):
 	nome = models.CharField(max_length=50)
-	#etapa_inicial = models.ForeignKey(Etapa,on_delete=models.SET_NULL,blank=True,null=True)
+	etapa_inicial = models.ForeignKey('Etapa',on_delete=models.SET_NULL,blank=True,null=True,related_name='primeira_etapa')
 
 	def __str__(self):
 		return self.nome
@@ -15,7 +15,7 @@ class Etapa(models.Model):
 	nome = models.CharField(max_length=50)
 	descricao = models.CharField(max_length=300)
 	prox_etapa = models.ForeignKey('self',on_delete=models.SET_NULL,blank=True,null=True)
-	#atividade_inicial = models.ForeignKey(Atividade,on_delete=models.SET_NULL,blank=True,null=True)
+	atividade_inicial = models.ForeignKey('Atividade',on_delete=models.SET_NULL,blank=True,null=True,related_name='primeira_atividade')
 	metodologia = models.ForeignKey(Metodologia,on_delete=models.CASCADE)
 
 	def __str__(self):
@@ -25,7 +25,7 @@ class Atividade(models.Model):
 	nome = models.CharField(max_length=50)
 	descricao = models.CharField(max_length=300)
 	prox_atividade = models.ForeignKey('self',on_delete=models.SET_NULL,blank=True,null=True)
-	etapa = models.ForeignKey(Etapa,on_delete=models.CASCADE)
+	etapa = models.ForeignKey(Etapa,on_delete=models.CASCADE,related_name='etapa_pai')
 
 	def __str__(self):
 		return self.nome
