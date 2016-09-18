6462777c63cd04c6f52c2b71ee7ce8e753bb2710
henrvelho <henrvelho@gmail.com>
2016-09-18 19:00:34 -0300

    modelo metodologia tirado

--- designare/metodologias/models.py
+++ designare/metodologias/models.py
@@ -62,24 +62,12 @@ class Atividade(models.Model):
 		return self.etapa.atividades.all().exclude(pk=self.pk)
 	
 	
-class Projeto(models.Model):
-	nome = models.CharField(max_length=50)
-	descricao = models.CharField(max_length=300)
-	metodologia = models.ForeignKey(
-			Metodologia,
-			on_delete=models.SET_NULL,
-			blank=True,
-			null=True
-	)
-	dt_inicio = models.DateField(False)
-	dt_fim = models.DateField(False)
-	capa = models.CharField(max_length=300)
-	estado = models.CharField(max_length=50)
+
 
 #class Execução(models.Model):
-#	data = models.
-#	usuario = models.
-#   recurso = models.
+#	data = models.DateField(False)
+#	usuario = models.CharField(max_length=50)
+#  recurso = models.
 
 #class Recurso(models.Model)
 #	acao = models.
