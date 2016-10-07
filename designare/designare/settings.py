3d7b03513351c22bb417cdeb26b94c846e4901ce
Daniel Vargas Muccillo <daniel.vargasmuccillo@gmail.com>
2016-10-07 16:27:59 -0300

    Adicionado template de esqueleto da aplicacao.
    Template esqueleto contem configuracoes do head e declaracao do body com
    arquivos javascript linkados.

--- designare/designare/settings.py
+++ designare/designare/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'designare.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "designare/templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
