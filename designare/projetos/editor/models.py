8d0da49bddd0a63c01d929ea1435cfffbfd61c18
henrvelho <henrvelho@gmail.com>
2016-11-01 14:19:09 -0200

    modelos recursos galeria e editor

--- designare/projetos/editor/models.py
+++ designare/projetos/editor/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Editor(models.Model):
+	titulo = models.CharField(max_length=50)
+	texto = models.CharField()
--- designare/projetos/galeria/models.py
+++ designare/projetos/galeria/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Galeria(models.Model):
+	nome = models.CharField(max_length=50)
+	descricao = models.CharField(max_length=300)
+
+	foto = models.ImageField(
+        upload_to='static/img/galeria/',
+    	default='static/img/projetos/capas/default.png',
