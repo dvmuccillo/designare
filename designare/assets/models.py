11375cccaa88290b954c98ebf1f0155ea2d90734
Daniel Vargas Muccillo <daniel.vargasmuccillo@gmail.com>
2016-12-22 11:54:13 -0200

    AssetsManager: Implementações e Cache
    - Implementada e utilizada função para limpar todas as listas
    - Implementado cache de arquivos json
    - Implementado uso do AssetsManager em metodologia, execucao e projeto.

--- designare/assets/models.py
+++ designare/assets/models.py
@@ -5,15 +5,27 @@ import json, os, io
 class AssetsManager(object):
     APP_BASE_DIR = os.path.join(os.getcwd(),'assets')
     BOWER_COMPONENTS_DIR = "/static/assets/bower_components/"
+    BOWER_OVERRIDE = {}
+    CACHED_JSON = {}
 
     css = []
     js = []
     bower_components = []
     plugins = []
 
+    def __init__(self):
+        path_to_json_file = os.path.join(self.APP_BASE_DIR + "/static/assets/bower.json")
+        self.BOWER_OVERRIDE = self.loadProperties(path_to_json_file)['overrides']        
+
     def loadProperties(self,path_to_json_file):
-        with io.open(path_to_json_file) as json_file:
-            properties = json.load(json_file)
+        if path_to_json_file in self.CACHED_JSON:
+            #print ('in cache ' + path_to_json_file +'\n')
+            properties = self.CACHED_JSON[path_to_json_file]
+        else:
+            #print ('loading ' + path_to_json_file +'\n')
+            with io.open(path_to_json_file) as json_file:
+                properties = json.load(json_file)
+                self.CACHED_JSON[path_to_json_file] = properties
         return properties
 
     def loadBowerPackageProperties(self,package):
@@ -23,11 +35,16 @@ class AssetsManager(object):
     def registerBower(self,package):
         if package not in self.bower_components:
             self.bower_components.append(package)
-            properties = self.loadBowerPackageProperties(package)
+            try:
+                properties = self.loadBowerPackageProperties(package)
+            except FileNotFoundError:
+                properties = self.BOWER_OVERRIDE[package]
             if 'dependencies' in properties:
                 for dependencie in properties['dependencies']:
                     self.registerBower(dependencie)
             package_dir = self.BOWER_COMPONENTS_DIR + package
+            if package in self.BOWER_OVERRIDE:
+                properties['main'] = self.BOWER_OVERRIDE[package]['main']
             self.allocateAssets(package_dir + "/",properties['main'])
 
 
@@ -61,3 +78,9 @@ class AssetsManager(object):
         for link in self.js:
             js_links += "<script src='"+link+"'></script>\n"
         return js_links
+
+    def clearAll(self):
+        self.bower_components = []
+        self.css = []
+        self.js = []
+        return ""
