9831d93f86e89c327b9e68d0643a84c034de0797
Daniel Vargas Muccillo <daniel.vargasmuccillo@gmail.com>
2016-12-27 17:45:27 -0200

    Accounts: alteração de informações pessoais.

--- designare/accounts/views.py
+++ designare/accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import logout,login,authenticate
 from django.shortcuts import get_object_or_404,render,redirect
 from django.http import HttpResponse,JsonResponse
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
     username = request.POST.get('username','')
@@ -19,11 +20,20 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("/")
-
+@login_required
 def user_profile(request):
     return render(request,"accounts/profile.html")
 
+@login_required
 def update_personal_info(request):
+    user = request.user
+    firstName = request.POST.get('firstName')
+    lastName = request.POST.get('lastName')
+    email = request.POST.get('email')
+    user.first_name = firstName
+    user.last_name = lastName
+    user.email = email
+    user.save()
     return JsonResponse({
             'success' : True,
         })
