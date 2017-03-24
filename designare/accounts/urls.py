7f8fc5daa93f1d5c19ba3ac1d21fbacfe221688e
Daniel Vargas Muccillo <daniel.vargasmuccillo@gmail.com>
2017-03-24 09:42:23 -0300

    Convite: url com código incluso.

--- designare/accounts/urls.py
+++ designare/accounts/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     url(r'^my/contacts/send-invite/$', views.user_send_invite, name='user_send_invite'),
     url(r'^my/contacts/resend-invite/$', views.user_resend_invite, name='user_resend_invite'),
     url(r'^redeem-invite/$', views.redeem_invite, name='redeem_invite'),
+    url(r'^redeem-invite/(?P<code>[A-Z0-9]+)/$', views.redeem_invite, name='redeem_invite_with_code'),
 ]
    
